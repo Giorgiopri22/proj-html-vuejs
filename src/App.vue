46c2fbb8a839448d767ee5139a6054059cc57f68
Giorgio22 <giorgioprizzi@gmail.com>
2023-05-12 13:15:02 +0200

    ee

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script >
 
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default{
+  name: "App",
+  components:{
+    AppHeader,
+    AppMain,
+    AppFooter,
+  }
+}
+
+
 </script>
 
 <template>
-  <h1>Ciao mondo</h1>
+  <AppHeader/>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,48 @@
+<script >
+
+export default{
+    name: "AppHeader",
+}
+
+</script>
+
+<template>
+    <header>
+        <div class="bg-info">
+            <section class="d-flex justify-content-between">
+                <div>
+                    <i class="fa-solid fa-clock"></i>
+                    <span>Open Hours: Mon-Sat -9:00-18:00</span>
+                </div>
+                <div class="info-social">
+                    <div>
+                        <i class="fa-solid fa-phone"></i>
+                        <span>+1(305)1234-5678</span>
+                        <i class="fa-solid fa-envelope"></i>
+                        <span>hello@example.com</span>
+                    </div>
+                </div>
+                <div>
+                    <i class="fa-brands fa-facebook"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-linkedin-in"></i>
+                </div>
+                
+            </section>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+.bg-info{   
+background-color: #230929;    
+    section{
+        width: 70%;
+        margin: 0 auto;
+        padding: 15px;
+    }
+}
+
+
+</style>
