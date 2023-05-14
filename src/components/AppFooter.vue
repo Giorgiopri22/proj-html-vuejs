94b7f7ac4bf86c3ff031741a496125e1289b4c12
Giorgio22 <giorgioprizzi@gmail.com>
2023-05-14 18:10:52 +0200

    3rd push

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,9 +7,158 @@ export default{
 </script>
 
 <template>
-    
+    <section>
+        <div class="box d-flex justify-content-between">
+            <div class="card-box">
+                <div class="my-5 p-5 contacts">
+                    <span>Example Inc.</span>
+                    <p class="my-2">
+                        A Functional HTML Template for Corporate & Business.
+                    </p>
+                    <div class="d-flex my-3 align-items-center">
+                        <div class="me-2 cerchio">
+                            <i class="fa-solid fa-phone"></i>
+                        </div>
+                        <div>
+                            <span>+1 (305) 1234-5678</span>
+                        </div>
+                    </div>
+                    <div class="d-flex my-3 align-items-center">
+                        <div class="me-2 cerchio">
+                            <i class="fa-solid fa-envelope"></i>
+                        </div>
+                        <div>
+                            <span>hello@example.com</span>
+                        </div>
+                    </div>
+                    <div class="d-flex my-3 align-items-center">
+                        <div class="me-2 cerchio">
+                            <i class="fa-solid fa-location-dot"></i>
+                        </div>
+                        <div>
+                            <span>Main Avenue, 987</span>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="my-3">GET IN TOUCH</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card-cover my-5 p-5">
+                <div>
+                    <h3>About</h3>
+                    <ul>
+                        <li class="d-flex align-items-center             justify-content-center">
+                            <i class="fa-solid fa-chevron-right me-2"></i>
+                            <span>The Company</span>
+                        </li>
+                        <li class="d-flex align-items-center             justify-content-center">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <span>The Company</span>
+                        </li>
+                        <li class="d-flex align-items-center             justify-content-center ">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <span>The Company</span>
+                        </li>
+                        <li class="d-flex align-items-center             justify-content-center">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <span>The Company</span>
+                        </li>
+                        <li class="d-flex align-items-center             justify-content-center">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <span>The Company</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="card-cover my-5 p-5">
+                <div>
+                    <h3>About</h3>
+                    <ul>
+                        <li class="d-flex align-items-center             justify-content-center">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <span>The Company</span>
+                        </li>
+                        <li class="d-flex align-items-center             justify-content-center">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <span>The Company</span>
+                        </li>
+                        <li class="d-flex align-items-center             justify-content-center ">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <span>The Company</span>
+                        </li>
+                        <li class="d-flex align-items-center             justify-content-center">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <span>The Company</span>
+                        </li>
+                        <li class="d-flex align-items-center             justify-content-center">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <span>The Company</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="card-cover my-5 p-5">
+                <div>
+                    <h3>About</h3>
+                    <ul>
+                        <li class="d-flex align-items-center             justify-content-center">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <span>The Company</span>
+                        </li>
+                        <li class="d-flex align-items-center             justify-content-center">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <span>The Company</span>
+                        </li>
+                        <li class="d-flex align-items-center             justify-content-center ">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <span>The Company</span>
+                        </li>
+                        <li class="d-flex align-items-center             justify-content-center">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <span>The Company</span>
+                        </li>
+                        <li class="d-flex align-items-center             justify-content-center">
+                            <i class="fa-solid fa-chevron-right "></i>
+                            <span>The Company</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
+section{
+    height: 900px;
+    background-image: url('../assets/img/bg-10.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+        .box{
+            width: 68%;
+            margin: 0 auto;
+                .card-box{
+                    width: calc(100% / 4 - 15px);
+                    color: white;
+                        button{
+                            width: 70%;
+                            color: white;
+                            background-color: inherit;
+                            border-radius: 4px;
+                            padding: 7px;
+                            border: 1px solid #2B7477;
+                        }                    
+                }
+        }
+        .card-cover{
+            border: 1px solid red;
+            border-radius: 10px;            
+                ul{
+                    list-style-type: none;
+                }
+            }
 
+}
 </style>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -8,20 +8,144 @@ export default{
 
 <template>
     <section>
-        <div class="my-3 p-4">
-            <span>
-                NEWSLETTER
-            </span>
-            <h2>
-                Know First
-            </h2>
-            <p>
-                Follow closely and receive content about our company and the news of the current market.
-            </p>
+        <div class="box d-flex justify-content-around">
+            <div class="my-5 p-4 form">
+                <span>
+                    SEND A MESSAGE
+                </span>
+                <h2>
+                    Get in Touch
+                </h2>
+                <p>
+                    We will respond to your message as soon as possible.
+                </p>
+                <div class="my-5">
+                    <div class="my-3">
+                        <input class="me-2 p-1 form-size" type="text" placeholder="  Name">
+                        <input class="p-1 form-size" type="email" placeholder="  Email">
+                    </div>
+                    <div class="my-3">
+                        <input class="me-2 p-1 form-size" type="number" placeholder="  Phone">
+                        <select class="p-1 form-size"  name="" id="">
+                            <option selected value="">More Info</option>
+                        </select>
+                    </div>
+                    <div>
+                        <textarea name="" id="text-area" placeholder="  Message"></textarea>
+                    </div>
+                    <div class="my-3">
+                        <button>SEND</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-5 p-5 contacts">
+                <span>Example Inc.</span>
+                <p class="my-2">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elite.
+                </p>
+                <p>
+                    Praesent diam lacus,dapibus sed imperdiet consectetur.
+                </p>
+                <div class="d-flex my-3 align-items-center">
+                    <div class="me-2 cerchio">
+                        <i class="fa-solid fa-phone"></i>
+                    </div>
+                    <div>
+                        <span>+1 (305) 1234-5678</span>
+                    </div>
+                </div>
+                <div class="d-flex my-3 align-items-center">
+                    <div class="me-2 cerchio">
+                        <i class="fa-solid fa-envelope"></i>
+                    </div>
+                    <div>
+                        <span>hello@example.com</span>
+                    </div>
+                </div>
+                <div class="d-flex my-3 align-items-center">
+                    <div class="me-2 cerchio">
+                        <i class="fa-solid fa-location-dot"></i>
+                    </div>
+                    <div>
+                        <span>Main Avenue, 987</span>
+                    </div>
+                </div>
+                <div>
+                    <button>VIEW MAP</button>
+                </div>
+            </div>
         </div>
     </section>
 </template>
 
 <style lang="scss" scoped>
-
+section{
+    background-color: #e5ebe5;
+    .box{
+        width: 70%;
+        margin: 0 auto;
+            .form{
+                width: 70%;
+                ::placeholder{
+                    color: #000000;
+                }
+                span{
+                    color: #088181;
+                    font-weight: 700;
+                }
+                h2{
+                    font-weight: 700;
+                    font-size: 42px;
+                }
+                .form-size{
+                    width: 40%;
+                    border: none;
+                    border-radius: 5px;
+                    background-color: #DDE2DB;
+                    
+                }
+                #text-area{
+                    width: 81%;
+                    height: 150px;
+                    border: none;
+                    border-radius: 5px;
+                    background-color: #DDE2DB;
+                    
+                    
+                }
+                button{
+                    padding: 7px;
+                    width: 8%;
+                    border: none;
+                    border-radius: 5px;
+                    color: white;
+                    background-color: #088181;
+                }
+            }
+            .contacts{
+                width: 25%;
+                    .cerchio{
+                        border-radius: 50%;
+                        background-color: #CFE2DE;
+                        width: 40px;
+                        height: 40px;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                       
+                        i{
+                            color: #338184;
+                        }
+                    }
+                    button{
+                        padding: 7px;
+                        width: 35%;
+                        border: 1px solid #35808F;
+                        color: #73878E;
+                        border-radius: 5px;
+                    }   
+                }
+        }
+}
 </style>
